c88437a3e1f025abedff21c6e23824307f8db794
hammadaslam1 <hammadaslam308@gmail.com>
2023-11-18 20:59:48 +0500

    connected database to club registration

--- src/screens/RegisterClub.jsx
+++ src/screens/RegisterClub.jsx
@@ -1,18 +1,96 @@
 /* eslint-disable prettier/prettier */
 /* eslint-disable react-native/no-inline-styles */
-import {View, Text, ScrollView, TouchableOpacity} from 'react-native';
+import {
+  View,
+  Text,
+  ScrollView,
+  TouchableOpacity,
+  Alert,
+  Image,
+} from 'react-native';
 import React, {useState} from 'react';
 import styles from '../styles/Styles';
 import SecondaryInput from '../components/inputs/SecondaryInput';
 import DatePicker from 'react-native-date-picker';
+import {useDispatch} from 'react-redux';
+import database from '@react-native-firebase/database';
+import auth from '@react-native-firebase/auth';
+import Spinner from 'react-native-loading-spinner-overlay';
 
-const RegisterClub = () => {
+const RegisterClub = props => {
+  const user = auth().currentUser;
+
+  // const dispatch = useDispatch();
+
+  const [clubName, setClubName] = useState();
+  const [city, setCity] = useState();
+  const [year, setYear] = useState(new Date().getFullYear());
+
   const [startDate, setStartDate] = useState(new Date());
   const [startOpen, setStartOpen] = useState(false);
-  const register = ['Club Name', 'City'];
+  const [isFilled, setIsFilled] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const register = [
+    {name: 'Club Name', set: setClubName},
+    {name: 'City', set: setCity},
+  ];
+
+  const handleClub = () => {
+    // setYear(startDate.getFullYear());
+    if (clubName && city && year) {
+      Alert.alert(
+        'Confirm Registration',
+        `Club name: ${clubName}\nCity: ${city}\nYear: ${year}`,
+        [
+          {
+            text: 'Cancel',
+            onPress: () => console.log('Cancel Pressed'),
+            style: 'cancel',
+          },
+          {
+            text: 'Register',
+            onPress: async () => {
+              setLoading(true);
+
+              const userRef = database().ref('/Clubs/' + user.uid);
+              userRef
+                .set({
+                  ownerID: user.uid,
+                  ownerName: user.displayName,
+                  Club_Name: clubName,
+                  City: city,
+                  Established_Year: year,
+                })
+                .then(() => {
+                  setLoading(false);
+                  Alert.alert(
+                    'Success',
+                    'Your Club has successfully been registered',
+                    [
+                      {
+                        text: 'Close',
+                        onPress: () => props.navigation.goBack(),
+                        style: 'cancel',
+                      },
+                    ],
+                  );
+                })
+                .catch(e => {
+                  Alert.alert('failed');
+                });
+            },
+          },
+        ],
+      );
+    } else {
+      setLoading(false);
+      setIsFilled(true);
+    }
+  };
   return (
     <ScrollView style={{backgroundColor: '#1058ad', minHeight: '100%'}}>
       <View>
+        <Spinner visible={loading} color="#3280cf" cancelable={true} />
         <Text
           style={[
             styles.text,
@@ -40,9 +118,13 @@ const RegisterClub = () => {
                     fontFamily: 'monospace',
                   },
                 ]}>
-                {i}
+                {i.name} <Text style={{color: '#0ff'}}>*</Text>
               </Text>
-              <SecondaryInput placeholder={i} />
+              <SecondaryInput
+                placeholder={i.name}
+                onChange={() => setIsFilled(false)}
+                onChangeText={i.set}
+              />
             </View>
           ))}
           <View>
@@ -57,7 +139,7 @@ const RegisterClub = () => {
                   fontFamily: 'monospace',
                 },
               ]}>
-              Established Year
+              Established Year <Text style={{color: '#0ff'}}>*</Text>
             </Text>
             <Text
               style={[
@@ -70,16 +152,34 @@ const RegisterClub = () => {
                   borderBottomWidth: 2,
                   paddingBottom: 8,
                   marginHorizontal: 120,
-                  marginTop: 20
+                  marginTop: 20,
                 },
               ]}
               onPress={() => setStartOpen(true)}>
-              {
-                startDate.getFullYear()}
+              {year}
             </Text>
           </View>
         </View>
-        <TouchableOpacity style={[styles.container, {marginTop: 100}]}>
+        {isFilled ? (
+          <Text
+            style={{
+              fontSize: 20,
+              fontFamily: 'monospace',
+              marginTop: 40,
+              textAlign: 'center',
+              fontWeight: 'bold',
+              color: '#0ff',
+            }}>
+            --- Please fill all fields ---
+          </Text>
+        ) : (
+          ''
+        )}
+        <TouchableOpacity
+          style={[styles.container, {marginTop: 30}]}
+          onPress={() => {
+            handleClub();
+          }}>
           <View
             style={[
               styles.button,
@@ -96,8 +196,7 @@ const RegisterClub = () => {
           date={startDate}
           maximumDate={new Date()}
           onConfirm={date => {
-            setStartDate(date);
-            //   alert(date.getFullYear());
+            setYear(date.getFullYear());
             setStartOpen(false);
           }}
           onCancel={() => {
