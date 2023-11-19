07150b53f8b2d31da1111092a4dada6933718184
hammadaslam1 <hammadaslam308@gmail.com>
2023-11-19 22:44:28 +0500

    modified create tournement screen and connected to database

--- src/components/homeCards/MatchCard.jsx
+++ src/components/homeCards/MatchCard.jsx
@@ -45,31 +45,62 @@ const MatchCard = () => {
               <Text style={[styles.topic, {marginBottom: 10, fontSize: 13}]}>
                 {/* {item.City} */} format, etc.
               </Text>
-              <View style={{flexDirection: 'row', alignItems: 'center'}}>
-                <Image
-                  source={require('../../assets/icons/pakistan.png')}
-                  style={{width: 15, height: 15, marginHorizontal: 2}}
-                />
-                <Text
-                  style={[styles.topic, {fontWeight: 'bold', fontSize: 15, marginHorizontal: 10}]}>
-                  {/* {item.Club_Name} */}Team A
-                </Text>
+              <View
+                style={{
+                  flexDirection: 'row',
+                  alignItems: 'center',
+                  justifyContent: 'space-between',
+                  marginVertical: 3
+                }}>
+                <View style={{flexDirection: 'row', alignItems: 'center'}}>
+                  <Image
+                    source={require('../../assets/icons/pakistan.png')}
+                    style={{width: 15, height: 15, marginHorizontal: 2}}
+                  />
+                  <Text
+                    style={[
+                      styles.topic,
+                      {fontWeight: 'bold', fontSize: 15, marginHorizontal: 10},
+                    ]}>
+                    {/* {item.Club_Name} */}Team A
+                  </Text>
+                </View>
+                <View style={{flexDirection: 'row', alignItems: 'center'}}>
+                  <Text style={[styles.topic , {color: '#fff', fontSize: 14, marginHorizontal: 3}]}>0.0</Text>
+                  <Text style={[styles.topic , {color: '#fff', fontSize: 14, marginHorizontal: 3}]}>(0.0)</Text>
+                </View>
               </View>
-              <View style={{flexDirection: 'row', alignItems: 'center'}}>
-                <Image
-                  source={require('../../assets/icons/pakistan.png')}
-                  style={{width: 15, height: 15, marginHorizontal: 2}}
-                />
-                <Text
-                  style={[styles.topic, {fontWeight: 'bold', fontSize: 15, marginHorizontal: 10}]}>
-                  {/* {item.Club_Name} */}Team B
-                </Text>
+              <View
+                style={{
+                  flexDirection: 'row',
+                  alignItems: 'center',
+                  justifyContent: 'space-between',
+                  marginVertical: 3
+                }}>
+                <View style={{flexDirection: 'row', alignItems: 'center'}}>
+                  <Image
+                    source={require('../../assets/icons/pakistan.png')}
+                    style={{width: 15, height: 15, marginHorizontal: 2}}
+                  />
+                  <Text
+                    style={[
+                      styles.topic,
+                      {fontWeight: 'bold', fontSize: 15, marginHorizontal: 10},
+                    ]}>
+                    {/* {item.Club_Name} */}Team B
+                  </Text>
+                </View>
+                <View style={{flexDirection: 'row', alignItems: 'center'}}>
+                  <Text style={[styles.topic , {color: '#fff', fontSize: 14, marginHorizontal: 3}]}>0.0</Text>
+                  <Text style={[styles.topic , {color: '#fff', fontSize: 14, marginHorizontal: 3}]}>(0.0)</Text>
+                </View>
               </View>
               <Text style={[styles.topic, {marginVertical: 10, fontSize: 13}]}>
                 {/* {item.City} */} Tournament Name
               </Text>
               <TouchableOpacity>
-                <Text style={[styles.topic, {fontSize: 15, fontWeight: 'bold'}]}>
+                <Text
+                  style={[styles.topic, {fontSize: 15, fontWeight: 'bold'}]}>
                   {/*{item.Established_Year} */} View Tournament {'>>'}
                 </Text>
               </TouchableOpacity>
--- src/screens/CreateTournament.jsx
+++ src/screens/CreateTournament.jsx
@@ -1,21 +1,127 @@
 /* eslint-disable prettier/prettier */
+/* eslint-disable no-unused-vars */
 /* eslint-disable react-native/no-inline-styles */
-import {View, Text, ScrollView, TouchableOpacity} from 'react-native';
+import {View, Text, ScrollView, TouchableOpacity, Alert} from 'react-native';
 import React, {useState} from 'react';
 import styles from '../styles/Styles';
 import SecondaryInput from '../components/inputs/SecondaryInput';
 import DatePicker from 'react-native-date-picker';
+import Spinner from 'react-native-loading-spinner-overlay';
+import auth from '@react-native-firebase/auth';
+import database from '@react-native-firebase/database';
 
-const CreateTournament = () => {
+const CreateTournament = props => {
   //   const [date, setDate] = useState(new Date());
   const [startDate, setStartDate] = useState(new Date());
   const [startOpen, setStartOpen] = useState(false);
   const [endDate, setEndDate] = useState(new Date());
   const [endOpen, setEndOpen] = useState(false);
-  const register = ['Tournament Name', 'Club Name', 'City', 'Season/Year'];
+  const bType = ['Tape Ball', 'Leather Ball'];
+  const [isType, setIsType] = useState('');
+  const [ballType, setBallType] = useState('');
+  const [isFilled, setIsFilled] = useState(false);
+  const [loading, setLoading] = useState(false);
+
+  const [tournament, setTournament] = useState('');
+  const [clubName, setClubName] = useState('');
+  const [city, setCity] = useState('');
+  const [sessionYear, setSessionYear] = useState('');
+  const [isLeather, setIsLeather] = useState(false);
+  const [isTennis, setIsTennis] = useState(true);
+  const register = [
+    {text: 'Tournament Name', fun: setTournament},
+    {text: 'Club Name', fun: setClubName},
+    {text: 'City', fun: setCity},
+    {text: 'Season/Year', fun: setSessionYear},
+  ];
+
+  const user = auth().currentUser;
+
+  const handleTour = () => {
+    if (ballType == 'Leather Ball') {
+      setIsLeather(true);
+      setIsFilled(false);
+    }
+    if (ballType == 'Tape Ball') {
+      setIsFilled(true);
+      setIsLeather(false);
+    }
+    if (
+      startDate &&
+      endDate &&
+      tournament &&
+      clubName &&
+      city &&
+      sessionYear &&
+      (isLeather || isTennis)
+    ) {
+      
+      Alert.alert(
+        'Confirm Tournament Creation',
+        `Club name: ${clubName}\nCity: ${city}\nSession: ${sessionYear}\nLeather: ${isLeather}\nTennis: ${isTennis}\nStart Date: ${startDate}\nEnd Date: ${endDate}\nTournament Name: ${tournament}`,
+        [
+          {
+            text: 'Cancel',
+            onPress: () => console.log('Cancel Pressed'),
+            style: 'cancel',
+          },
+          {
+            text: 'Register',
+            onPress: async () => {
+              setLoading(true);
+              const userRef = database().ref('/Tournaments').push();
+              userRef
+                .set({
+                  ownerID: user.uid,
+                  Club_Name: clubName,
+                  City: city,
+                  Tournament: tournament,
+                  Session: sessionYear,
+                  isLeather: isLeather,
+                  isTennis: isTennis,
+                  startDate:
+                    startDate.getDate() +
+                    ' - ' +
+                    startDate.getMonth() +
+                    ' - ' +
+                    startDate.getFullYear(),
+                  endDate:
+                    endDate.getDate() +
+                    ' - ' +
+                    endDate.getMonth() +
+                    ' - ' +
+                    endDate.getFullYear(),
+                })
+                .then(() => {
+                  setLoading(false);
+                  Alert.alert(
+                    'Success',
+                    'Your Tournament has successfully been created',
+                    [
+                      {
+                        text: 'Close',
+                        onPress: () => props.navigation.goBack(),
+                        style: 'cancel',
+                      },
+                    ],
+                  );
+                })
+                .catch(() => {
+                  Alert.alert('failed');
+                });
+            },
+          },
+        ],
+      );
+    } else {
+      setLoading(false);
+      setIsFilled(true);
+    }
+  };
   return (
     <ScrollView style={{backgroundColor: '#1058ad', minHeight: '100%'}}>
       <View>
+        <Spinner visible={loading} color="#3280cf" cancelable={true} />
         <View style={{marginTop: 30}}>
           {register.map((data, i) => (
             <View key={'' + i}>
@@ -30,9 +136,13 @@ const CreateTournament = () => {
                     fontFamily: 'monospace',
                   },
                 ]}>
-                {data}
+                {data.text}
               </Text>
-              <SecondaryInput placeholder={data} />
+              <SecondaryInput
+                placeholder={data.text}
+                onChangeText={data.fun}
+                onChange={() => setIsFilled(false)}
+              />
             </View>
           ))}
           <View
@@ -103,6 +213,50 @@ const CreateTournament = () => {
                 endDate.getFullYear()}
             </Text>
           </View>
+          <Text
+            style={[
+              styles.text,
+              {
+                fontSize: 20,
+                alignSelf: 'flex-start',
+                marginTop: 25,
+                marginRight: 20,
+                fontFamily: 'monospace',
+              },
+            ]}>
+            Ball Type
+          </Text>
+          <View style={[styles.innerRecord, {marginTop: 25}]}>
+            {bType.map((ball, i) => (
+              <TouchableOpacity
+                key={i}
+                activeOpacity={0.7}
+                onPress={() => {
+                  setBallType(ball);
+                  setIsType(ball);
+                }}>
+                <View
+                  style={{
+                    padding: 8,
+                    width: 100,
+                    flex: 1,
+                    alignItems: 'center',
+                    borderRadius: 10,
+                    elevation: isType == ball ? 5 : 0,
+                    backgroundColor: isType == ball ? '#fff' : '#1058ad',
+                  }}>
+                  <Text
+                    style={{
+                      color: isType == ball ? '#1058ad' : '#fff',
+                      fontWeight: 'bold',
+                      fontSize: 16,
+                    }}>
+                    {ball}
+                  </Text>
+                </View>
+              </TouchableOpacity>
+            ))}
+          </View>
           <DatePicker
             modal
             mode="date"
@@ -136,7 +290,24 @@ const CreateTournament = () => {
             }}
           />
         </View>
-        <TouchableOpacity style={[styles.container, {marginTop: 100}]}>
+        {isFilled ? (
+          <Text
+            style={{
+              fontSize: 20,
+              fontFamily: 'monospace',
+              marginTop: 40,
+              textAlign: 'center',
+              fontWeight: 'bold',
+              color: '#0ff',
+            }}>
+            --- Please fill all fields ---
+          </Text>
+        ) : (
+          ''
+        )}
+        <TouchableOpacity
+          style={[styles.container, {marginVertical: 60}]}
+          onPress={() => handleTour()}>
           <View
             style={[
               styles.button,
